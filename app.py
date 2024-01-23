19c7400d6e2bad57380c2f45eda0cc7b8b91692f
Kritsada Masanthia <42602382+james20260@users.noreply.github.com>
2024-01-23 16:48:00 +0700

    .

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ st.write('Harness the power of generative AI to create insightful company report
 create_report_form = st.form(key='create_report')
 input_url = create_report_form.text_input(label='Company Webpage URL', placeholder='', value='', disabled=False,
                                           help='This can be any webpage that provides basic information about the company (e.g. https://fluxus.io).')
+create_report_submitted = create_report_form.form_submit_button(label='Create a New Report', disabled=False)
 
-# Expander inside the form
-with create_report_form.expander("Customize Workflow (Optional)"):
+# Expander for customizing workflow (optional)
+with st.expander("Customize Workflow (Optional)"):
     workflow_form = st.form(key='workflow')
     submit_button = workflow_form.form_submit_button(label='Hit me')
 
-create_report_submitted = create_report_form.form_submit_button(label='Create a New Report', disabled=False)
 if create_report_submitted:
     with st.spinner('creating report...'):
         create_new_report(create_report_form, input_url)
@@ -29,6 +29,7 @@ get_report_form = st.form(key='get_report')
 report_id = get_report_form.text_input(label='Report ID', placeholder='', value='', disabled=False,
                                        help="All reports have a unique identifier assigned to them. This was provided to you in the response when creating a new report.")
 get_report_submitted = get_report_form.form_submit_button(label='Get an Existing Report', disabled=False)
+
 if get_report_submitted:
     with st.spinner('retrieving report...'):
         get_existing_report(get_report_form, report_id)
