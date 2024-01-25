17ef5b3a5eb4d07d88a0e1538a8f75bce122636c
Kritsada Masanthia <42602382+james20260@users.noreply.github.com>
2024-01-25 13:25:48 +0700

    .

--- app.py
+++ app.py
@@ -1,141 +1,66 @@
 import streamlit as st
-from api_handler import create_new_report, get_existing_report
 
-st.set_page_config(page_title='AI Company Reports')
+def workflow_checkbox(column, label, key):
+    return column.checkbox(label=label, key=key)
 
-hide_streamlit_style = "<style> footer { visibility: hidden; } </style>"
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+def workflow_advanced_options(model_type, model_options, use_multiprompt):
+    selected_model = st.selectbox("Model", model_options)
+    if use_multiprompt:
+        p_human = st.text_input("p_human")
+        p_task_context = st.text_input("p_task_context")
+        p_tone_context = st.text_input("p_tone_context")
+        p_data = st.text_input("p_data")
+        p_task_description = st.text_input("p_task_description")
+        p_example = st.text_input("p_example")
+        p_conversation_history = st.text_input("p_conversation_history")
+        p_thought_process = st.text_input("p_thought_process")
+        p_formatting = st.text_input("p_formatting")
+        p_assistant = st.text_input("p_assistant")
+    else:
+        prompt = st.text_input("Prompt")
+        return selected_model, prompt
 
-st.subheader('Company Report Generator', divider='rainbow')
-st.write('Harness the power of generative AI to create insightful company reports')
-st.text_input(label='Company Webpage URL',
-              placeholder='',
-              value='',
-              disabled=False,
-              help='This can be any webpage that provides basic information about the company (e.g. https://fluxus.io).')
+def create_workflow_step_options(label, key):
+    check = st.checkbox(label=label, key=key)
+    if check:
+        model_provider = st.selectbox("Model Provider", ["Bedrock", "OpenAI"])
+        if model_provider == "Bedrock":
+            model_options = ["anthropic.claude-v1", "anthropic.claude-v2", "anthropic.claude-instant-v1"]
+            use_multiprompt = st.toggle("Granular-prompt")
+            return model_provider, workflow_advanced_options(model_provider, model_options, use_multiprompt)
+        else:
+            return model_provider, None
+    return None, None
 
-with st.expander("Customize Workflow (Optional)"):
-    t1, t2 , t3 = st.tabs(["Simple", "Advanced", "Fully Customize"])
-    with t1:
-        c1, c2, c3, c4 = st.columns(4)
-        
-        with c1:
-            st.checkbox(label="Introduction", key="simple_in")
-        with c2:
-            st.checkbox(label="Business Health", key="simple_bh")
-        with c3:
-            st.checkbox(label="Audiences", key="simple_au")
-        with c4:
-            st.checkbox(label="Competitors", key="simple_co")
-            
-        save_t1 = st.button('Save Selection')
+def create_workflow():
+    st.set_page_config(page_title='AI Company Reports')
+    hide_streamlit_style = "<style> footer { visibility: hidden; } </style>"
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-    with t2:
-        check_in = st.checkbox(label="Introduction", key="advance_in")
-        if check_in:
-            in_model_provider = st.selectbox("Model Provider", ["Bedrock", "OpenAI"])
-            if in_model_provider == "Bedrock":
-                in_model =  st.selectbox("Model", ["anthropic.claude-v1", "anthropic.claude-v2","anthropic.claude-instant-v1"])
-                in_multiprompt = st.toggle("Granular-prompt")
-                if not in_multiprompt:
-                    in_prompt = st.text_input("Prompt")
-                else:
-                    in_p_human = st.text_input("p_human")
-                    in_p_task_context = st.text_input("p_task_context")
-                    in_p_tone_context = st.text_input("p_tone_context")
-                    in_p_data = st.text_input("p_data")
-                    in_p_task_description = st.text_input("p_task_description")
-                    in_p_example = st.text_input("p_example")
-                    in_p_conversation_history = st.text_input("p_conversation_history")
-                    in_p_thought_process = st.text_input("p_thought_process")
-                    in_p_formatting = st.text_input("p_formatting")
-                    in_p_assistant = st.text_input("p_assistant")
-        check_bh = st.checkbox(label="Business health", key="advance_bh")
-        if check_bh:
-            bh_model_provider = st.selectbox("Model Provider", ["Bedrock", "OpenAI"])
-            if bh_model_provider == "Bedrock":
-                bh_model =  st.selectbox("Model", ["anthropic.claude-v1", "anthropic.claude-v2","anthropic.claude-instant-v1"])
-                bh_multiprompt = st.toggle("Granular-prompt")
-                if not bh_multiprompt:
-                    bh_prompt = st.text_input("Prompt")
-                else:
-                    bh_p_human = st.text_input("p_human")
-                    bh_p_task_context = st.text_input("p_task_context")
-                    bh_p_tone_context = st.text_input("p_tone_context")
-                    bh_p_data = st.text_input("p_data")
-                    bh_p_task_description = st.text_input("p_task_description")
-                    bh_p_example = st.text_input("p_example")
-                    bh_p_conversation_history = st.text_input("p_conversation_history")
-                    bh_p_thought_process = st.text_input("p_thought_process")
-                    bh_p_formatting = st.text_input("p_formatting")
-                    bh_p_assistant = st.text_input("p_assistant")
+    st.subheader('Company Report Generator', divider='rainbow')
+    st.write('Harness the power of generative AI to create insightful company reports')
+    st.text_input(label='Company Webpage URL',
+                  placeholder='',
+                  value='',
+                  disabled=False,
+                  help='This can be any webpage that provides basic information about the company (e.g. https://fluxus.io).')
 
-        
+    with st.expander("Customize Workflow (Optional)"):
+        t1, t2, t3 = st.tabs(["Simple", "Advanced", "Fully Customize"])
 
+        with t1:
+            c1, c2, c3, c4 = st.columns(4)
+            workflow_checkbox(c1, "Introduction", "simple_in")
+            workflow_checkbox(c2, "Business Health", "simple_bh")
+            workflow_checkbox(c3, "Audiences", "simple_au")
+            workflow_checkbox(c4, "Competitors", "simple_co")
+            save_t1 = st.button('Save Selection')
 
+        with t2:
+            in_model_provider, in_options = create_workflow_step_options("Introduction", "advance_in")
+            bh_model_provider, bh_options = create_workflow_step_options("Business health", "advance_bh")
 
-# # Form to create a new report
-# create_report_form = st.form(key='create_report')
-# input_url = create_report_form.text_input(label='Company Webpage URL', placeholder='', value='', disabled=False,
-#                                           help='This can be any webpage that provides basic information about the company (e.g. https://fluxus.io).')
-# create_report_submitted = create_report_form.form_submit_button(label='Create a New Report', disabled=False)
+        # Continue for other tabs if needed
 
-# # Expander for customizing workflow (optional)
-# with st.expander("Customize Workflow (Optional)"):
-#     workflow_form = st.form(key='workflow')
-#     get_workflow = workflow_form.form_submit_button(label='Hit me')
-#     create_workflow = workflow_form.form_submit_button(label='Hit me')
-
-# if create_report_submitted:
-#     with st.spinner('creating report...'):
-#         create_new_report(create_report_form, input_url)
-
-# # Form to query an existing report
-# get_report_form = st.form(key='get_report')
-# report_id = get_report_form.text_input(label='Report ID', placeholder='', value='', disabled=False,
-#                                        help="All reports have a unique identifier assigned to them. This was provided to you in the response when creating a new report.")
-# get_report_submitted = get_report_form.form_submit_button(label='Get an Existing Report', disabled=False)
-
-# if get_report_submitted:
-#     with st.spinner('retrieving report...'):
-#         get_existing_report(get_report_form, report_id)
-
-# import streamlit as st
-# from api_handler import create_new_report, get_existing_report
-
-# st.set_page_config(page_title='AI Company Reports')
-
-# hide_streamlit_style = "<style> footer { visibility: hidden; } </style>"
-# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
-# st.subheader('Company Report Generator', divider='rainbow')
-# st.write('Harness the power of generative AI to create insightful company reports')
-
-# # Form to create a new report
-# create_report_form = st.form(key='create_report')
-# input_url = create_report_form.text_input(label='Company Webpage URL', placeholder='', value='', disabled=False,
-#                                           help='This can be any webpage that provides basic information about the company (e.g. https://fluxus.io).')
-# create_report_submitted = create_report_form.form_submit_button(label='Create a New Report', disabled=False)
-
-# # Expander for customizing workflow (optional)
-# with st.expander("Customize Workflow (Optional)"):
-#     workflow_form = st.form(key='workflow')
-#     get_workflow = workflow_form.form_submit_button(label='Hit me')
-#     create_workflow = workflow_form.form_submit_button(label='Hit me')
-
-# if create_report_submitted:
-#     with st.spinner('creating report...'):
-#         create_new_report(create_report_form, input_url)
-
-# # Form to query an existing report
-# get_report_form = st.form(key='get_report')
-# report_id = get_report_form.text_input(label='Report ID', placeholder='', value='', disabled=False,
-#                                        help="All reports have a unique identifier assigned to them. This was provided to you in the response when creating a new report.")
-# get_report_submitted = get_report_form.form_submit_button(label='Get an Existing Report', disabled=False)
-
-# if get_report_submitted:
-#     with st.spinner('retrieving report...'):
-#         get_existing_report(get_report_form, report_id)
-
-
-
+if __name__ == "__main__":
+    create_workflow()
