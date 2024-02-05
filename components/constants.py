0f4b3896cb0a29c5f97806f73c31a4dae437ba44
Kritsada Masanthia <42602382+james20260@users.noreply.github.com>
2024-02-05 16:27:24 +0700

    .

--- components/constants.py
+++ components/constants.py
@@ -5,7 +5,7 @@ API_BASE_URL = st.secrets['api_base_url']
 API_KEY = st.secrets['api_key']
 GENERATE_WORKFLOW_ENDPOINT = f'{API_BASE_URL}/create-workflow'
 GENERATE_COMPANY_REPORT_ENDPOINT = f'{API_BASE_URL}/generate-company-report'
-GET_PROGRESS_ENDPOINT = f'{API_BASE_URL}/get-progress?transactionID='
+GET_PROGRESS_ENDPOINT = f'{API_BASE_URL}/get-report?transactionID='
 LOCAL_HOST = True
 IGNORE_AUTHORIZATION_GROUP = True
 REMOVE_AUTHENTICATION = True
