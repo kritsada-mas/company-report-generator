1722cd0ad0b15741c74ea0f6049ab7bb59dd3482
Kritsada Masanthia <42602382+james20260@users.noreply.github.com>
2024-02-05 16:17:01 +0700

    .

--- pages/1_company_report_generator.py
+++ pages/1_company_report_generator.py
@@ -5,7 +5,7 @@ from components.constants import IGNORE_AUTHORIZATION_GROUP, REMOVE_AUTHENTICATI
 from components.api_handler import upload_workflow, create_new_report, get_existing_report
 
 # Initialize workflow_name globally using st.session_state
-st.session_state['workflow_name'] = "default"
+# st.session_state['workflow_name'] = "default"
 
 # Page configuration
 st.set_page_config(page_title="Company Report Generator", page_icon="📈")
