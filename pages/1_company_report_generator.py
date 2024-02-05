ce4371c8931d3d69885e6209201c2ed5528d3717
Kritsada Masanthia <42602382+james20260@users.noreply.github.com>
2024-02-05 16:19:20 +0700

    .

--- pages/1_company_report_generator.py
+++ pages/1_company_report_generator.py
@@ -61,10 +61,16 @@ if (
     
     create_report_form = st.form(key = 'create_report')
     input_url = create_report_form.text_input(label = 'Company Webpage URL', placeholder = '', value = '', disabled = False, help = 'This can be any webpage that provides basic information about the company (e.g. https://fluxus.io).')
-    st.write(st.session_state['workflow_name'])
     create_report_submitted = create_report_form.form_submit_button(label = 'Create a New Report', disabled = False)
     if create_report_submitted: 
         with st.spinner('creating report...'): create_new_report(st.session_state['workflow_name'], create_report_form, input_url)
+        
+    # form to query an existing report
+    get_report_form = st.form(key = 'get_report')
+    report_id = get_report_form.text_input(label = 'Report ID', placeholder = '', value = '', disabled = False, help = "All reports have a unique identifier assigned to them. This was provided to you in the response when creating a new report.")
+    get_report_submitted = get_report_form.form_submit_button(label = 'Get an Existing Report', disabled = False)
+    if get_report_submitted:
+        with st.spinner('retrieving report...'): get_existing_report(get_report_form, report_id) 
     
 
 else:
