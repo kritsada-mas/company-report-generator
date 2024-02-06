3f5c48da536676e9452cd14a398a53866abb4fba
Kritsada Masanthia <42602382+james20260@users.noreply.github.com>
2024-02-06 16:41:25 +0700

    .

--- app.py
+++ app.py
@@ -2,30 +2,6 @@ import streamlit as st
 import components.authenticate as authenticate
 from components.constants import REMOVE_AUTHENTICATION
 
-def switch_page(page_name: str):
-    from streamlit import _RerunData, _RerunException
-    from streamlit.source_util import get_pages
-
-    def standardize_name(name: str) -> str:
-        return name.lower().replace("_", " ")
-    
-    page_name = standardize_name(page_name)
-
-    pages = get_pages("app.py")  # OR whatever your main page is called
-
-    for page_hash, config in pages.items():
-        if standardize_name(config["page_name"]) == page_name:
-            raise _RerunException(
-                _RerunData(
-                    page_script_hash=page_hash,
-                    page_name=page_name,
-                )
-            )
-
-    page_names = [standardize_name(config["page_name"]) for config in pages.values()]
-
-    raise ValueError(f"Could not find page {page_name}. Must be one of {page_names}")
-
 st.set_page_config(
     page_title="Home",
     page_icon="images/logo.jpg",
@@ -50,5 +26,3 @@ else:
 # Additional content on the home page
 st.write("Explore the latest advancements in AI/ML and unlock the potential of Fluxus technologies.")
 
-if st.button("Company Report Generator"):
-    switch_page("Company Report Generator")
