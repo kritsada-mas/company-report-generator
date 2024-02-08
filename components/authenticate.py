0716d57856774f3d351f054734dd5939b0aa758b
Kritsada Masanthia <42602382+james20260@users.noreply.github.com>
2024-02-08 11:11:12 +0700

    .

--- components/authenticate.py
+++ components/authenticate.py
@@ -171,7 +171,7 @@ def get_user_cognito_groups(id_token):
     if id_token != "":
         header, payload, signature = id_token.split(".")
         printable_payload = base64.urlsafe_b64decode(pad_base64(payload))
-        logging.infor("printable_payload")
+        logging.info("printable_payload")
         payload_dict = json.loads(printable_payload)
         try:
             user_cognito_groups = list(dict(payload_dict)["cognito:groups"])
