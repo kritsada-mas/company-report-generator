94e3e9ccb3a6ea72a30d6ca60ef0f4f5d2740239
Kritsada Masanthia <42602382+james20260@users.noreply.github.com>
2024-02-08 10:43:16 +0700

    .

--- app.py
+++ app.py
@@ -27,11 +27,18 @@ if st.session_state["authenticated"]:
     # Additional content on the home page
     st.write("Explore the latest advancements in AI/ML and unlock the potential of Fluxus technologies.")
 
+    blank_row = st.columns(3)
     row1 = st.columns(3)
-    r2c1, r2c2, r2c3 = st.columns(3)
 
-    for col in row1:
-        tile = col.container(height=60,border=False)
+    for col in blank_row:
+        col.container(height=60,border=False)
         
-    tile = r2c1.container(height=90)
-    tile.page_link("pages/1_company_report_generator.py", label="Company Report\n\nGenerator", use_container_width=True)
+    tile1 = row1[0].container(height=90)
+    tile1.page_link("pages/1_company_report_generator.py", label="Company Report\n\nGenerator", use_container_width=True)
+    
+    tile2 = row1[0].container(height=90)
+    tile2.page_link("pages/2_server_log_generator.py", label="Server Log\n\nGenerator", use_container_width=True)
+    
+    tile3 = row1[0].container(height=90)
+    tile3.page_link("pages/3_temp.py", label="Document PII\n\nDetector", use_container_width=True)
+
