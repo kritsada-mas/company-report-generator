3f8947e66b5433c05c88c43e9a53ae2cdfb4518d
Kritsada Masanthia <42602382+james20260@users.noreply.github.com>
2024-02-09 15:15:55 +0700

    .

--- pages/1_company_report_generator.py
+++ pages/1_company_report_generator.py
@@ -43,12 +43,12 @@ if (
                 simple_in = st.checkbox(label="Introduction", key="simple_in", value=True)
                 simple_bh = st.checkbox(label="Business Health", key="simple_bh", value=True)
                 simple_au = st.checkbox(label="Audiences", key="simple_au", value=True)
-                simple_co = st.checkbox(label="Competitors", key="simple_co", value=True)
+                # simple_co = st.checkbox(label="Competitors", key="simple_co", value=True)
                 saved_config = {
                         "Introduction": simple_in,
                         "Business Health": simple_bh,
                         "Audiences": simple_au,
-                        "Competitors": simple_co,
+                        # "Competitors": simple_co,
                     }
                 
                 if st.button("Confirm Selection"):
@@ -56,6 +56,7 @@ if (
                     with st.spinner('Uploading workflow...'): upload_workflow(workflow_body)
             with t2:
                 st.write("Report Template is currently in development")
+                st.file_uploader("Report Template")
         with st.expander("History"):
             st.write("History is currently in development")
     
