bd9e229f4b381faa5d13519f1c779608ba38d5db
Kritsada Masanthia <42602382+james20260@users.noreply.github.com>
2024-02-09 18:22:08 +0700

    .

--- app.py
+++ app.py
@@ -27,19 +27,19 @@ if st.session_state["authenticated"]:
     # Additional content on the home page
     st.write("Explore the latest advancements in AI/ML and unlock the potential of Fluxus technologies.")
 
-    # blank_row = st.columns(3)
-    # row1 = st.columns(3)
+    blank_row = st.columns(3)
+    row1 = st.columns(3)
     
 
-    # for col in blank_row:
-    #     col.container(height=60,border=False)
+    for col in blank_row:
+        col.container(height=60,border=False)
         
-    # tile1 = row1[0].container(height=90)
-    # tile1.page_link("pages/1_company_report_generator.py", label="Company Report\n\nGenerator", use_container_width=True)
+    tile1 = row1[0].container(height=90)
+    tile1.page_link("pages/1_company_report_generator.py", label="Company Report\n\nGenerator", use_container_width=True)
     
-    # tile2 = row1[1].container(height=90)
-    # tile2.page_link("pages/2_server_log_generator.py", label="Server Log\n\nGenerator", use_container_width=True)
+    tile2 = row1[1].container(height=90)
+    tile2.page_link("pages/2_server_log_generator.py", label="Server Log\n\nGenerator", use_container_width=True)
     
-    # tile3 = row1[2].container(height=90)
-    # tile3.page_link("pages/3_temp.py", label="Document PII\n\nDetector", use_container_width=True)
+    tile3 = row1[2].container(height=90)
+    tile3.page_link("pages/3_temp.py", label="Document PII\n\nDetector", use_container_width=True)
 
